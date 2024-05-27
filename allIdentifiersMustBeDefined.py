37e24e8bdddc8de4142237df13f6a494b210ca45
Roman Njoroge <roman.njoroge@njuguna.com>
2024-05-27 19:56:10 +0300

    feat: To implement this part

---
+++ allIdentifiersMustBeDefined.py
@@ -0,0 +1,9 @@
+def allIdentifiersMustBeDefined():
+    # Look through code and get where an identifier is used
+
+    # Check if identifier is been used in variable definition or function definition
+
+    # If not check if the variable has been defined earlier
+
+    # If not defined earlier throw error
+    pass
