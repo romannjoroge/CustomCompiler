2d8dfa3d300e6fa8032c5f0e15dc7fba524091cc
Roman Njoroge <roman.njoroge@njuguna.com>
2024-05-28 12:47:10 +0300

    feat: Checking if function names are declared before they are used and also using function arguements while checking if item has been defined

--- allIdentifiersMustBeDefined.py
+++ allIdentifiersMustBeDefined.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from tree import MyTree
 
 def allIdentifiersMustBeDefined(tokens: List[List], tree: 'MyTree'):
@@ -37,6 +37,57 @@ def checkIfIdetifierIsBeingDefined(tree: 'MyTree', lexeme: str, position: int) -
                         continue
                 else:
                     continue
+        # Check if item is a function definition
+        elif 'FUNCTION_DEFINITION' in key:
+            # Check if identifier is one of the children
+            for child in value:
+                # Check if the identifier is the name of the function
+                if "ID" in child and lexeme in child:
+                    # Check if position is the same
+                    if position == child[1]:
+                        return True
+                    else:
+                        continue
+                
+                # Check if its one of the arguements
+                else:
+                    # Get arguements
+                    arguements = getListOfArguements(function_def_position=key[1], tree=tree)
+                    for arg in arguements:
+                        if arg[0] == lexeme and arg[1] == position:
+                            return True
+                        else:
+                            continue
         else:
             continue
     return False
+
+def getListOfArguements(function_def_position: int, tree: 'MyTree') -> List[Tuple[str, int]]:
+    # Get the function definition
+    for key, value in tree.data.items():
+        # Check if item is the function definition we are looking for
+        if 'FUNCTION_DEFINITION' in key and function_def_position in key:
+            # Get the arguements child
+            arguements_child = ()
+            for child in value:
+                if 'ARGUMENTS' in child:
+                    arguements_child = child
+
+                    # Get children of arguements child
+                    arguement_children = tree.data[arguements_child]
+
+                    # If children is empty
+                    if len(arguement_children) == 0:
+                        return []
+                    else:
+                        arguements = []
+                        for entry in arguement_children:
+                            if "ID" in entry:
+                                arguements.append((entry[2], entry[1]))
+                        return arguements
+                else:
+                    continue
+        else:
+            continue
+
+    return []
--- test4.py
+++ test4.py
@@ -1,6 +1,6 @@
 from scanner import Scanner
 from parser_1 import parserv2
-from allIdentifiersMustBeDefined import allIdentifiersMustBeDefined
+from allIdentifiersMustBeDefined import allIdentifiersMustBeDefined, getListOfArguements
 
 tokens = Scanner()
 tree, labels = parserv2(tokens, [])
@@ -9,6 +9,11 @@ print(f"\n\n----------------TREE-------------------\n\n")
 print("Tree is =>", tree.data)
 
 print(f"\nSEMANTIC ANALYSIS\n")
+
+# Test if getListOfArguements works
+# arguements = getListOfArguements(function_def_position=18, tree=tree)
+# print("Gotten arguements is => ", arguements)
+
 # Check if variable definition is there
 allIdentifiersMustBeDefined(tokens=tokens, tree=tree)
 print("All Identifiers are used after being defined")  
