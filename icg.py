34296714997d39d2bc4dbacf1de7fb94ad5b8464
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-05-30 13:38:16 +0300

    feat: variable definition

--- icg.py
+++ icg.py
@@ -8,6 +8,7 @@ def icg():
     tree, labels, x = parserv2(tokens, [])
     quadruples  =[]
     temp_variables = 0
+    label_vars = 0
 
     index = 0
 
@@ -28,6 +29,7 @@ def icg():
 
         #Look for the position of token in tree
         pos_token = (token[0], index, token[1])
+        print("pos_token####", pos_token)
         for key, value in tree.data.items():
             parent = key
             #Handle parent accordingly
@@ -35,28 +37,93 @@ def icg():
                 #check its parent
                 print("key: ", key)
                 print("pos_token: ", pos_token)
+                if key[0] =='VARIABLE_DEFINITION':
+                    print("handling VARIABLE_DEFINITION")
+                    #Handle parent accordingly
+                    for val in value:
 
-                #Handle parent accordingly
-                for val in value:
-                    if val[0] == 'AE':
-                        T = tree.data[val][0]
-                        print("Tree T", T)
-                        F = tree.data[T][0]
-                        final_value = tree.data[F][0][2]
-                        
-                        print("Tree vall", tree.data[val])
-                        
-                        print("final_value", final_value)
-                        quad = f"(ASSIGN, {token[1]}, {final_value})"
-                        quadruples.append(quad)
+                        #Varable definition
+                        if val[0] == 'AE':
+                            T = tree.data[val][0]
+                            print("Tree T", T)
+                            F = tree.data[T][0]
+                            final_value = tree.data[F][0][2]
 
-                        #Jump to the end of the parent in the token list
-                        index = parent[1] + 1
-                        print("index", index)
-                        break
-                   
+                            print("Tree vall", tree.data[val])
+
+                            print("final_value", final_value)
+                            quad = f"(ASSIGN, {token[1]}, {final_value})"
+                            quadruples.append(quad)
+
+                            #Jump to the end of the parent in the token list
+                            index = parent[1] + 1
+                            print("index", index)
+                            break
+                if key[0] == 'ASSIGNMENT':
+                    print(" handling ASSIGNMENT")
+                    for val in value:
+                        #Handle assignment
+                        #eg. (ASSIGN, $d, 4)
+                        if val[0] == 'INT' or val[0] == 'FLOAT' or val[0] == 'STR':
+                            quad = f"(ASSIGN, {token[1]}, {val[2]})"
+                            quadruples.append(quad)
+                            index = parent[1] + 1
+                            break
+                        if val[0] == 'AE':
+                            T = tree.data[val][0]
+                            print("Tree T", T)
+                            F = tree.data[T][0]
+                            final_value = tree.data[F][0][2]
+
+                            print("Tree vall", tree.data[val])
+
+                            print("final_value", final_value)
+                            quad = f"(ASSIGN, {token[1]}, {final_value})"
+                            quadruples.append(quad)
+
+                            #Jump to the end of the parent in the token list
+                            index = parent[1] + 1
+                            print("index", index)
+                            break
+                            
+                if key[0] == 'IF':
+                    print("handling IF")
+                    for val in value:
+                        if val[0] == 'LE':
+                            le1 = tree.data[val][0]
+                            le2 = tree.data[le1][0]
+                            le3 = tree.data[le2][0]
+                            le4 = tree.data[le3][0]
+                            le5 = tree.data[le4][0]
+                            if_expression = tree.data[le5][0][2]
+                            temp = f"t{temp_variables}"
+                            temp_variables += 1
+                            quad = f"(ASSIGN, {temp}, {if_expression})"
+                            quadruples.append(quad)
+                            #Handling LE in if
+                            temp2 = f"t{temp_variables}"
+                            temp_variables += 1
+                            quad = f"(NOT, {temp2}, {temp})"
+                            quadruples.append(quad)
+                            label = f"L{label_vars}"
+                            quad = f"(IF, {temp2}, {label})"
+                            quadruples.append(quad)
+                            #TODO:Handle the body
+                            quad = f"(LABEL, {label})"
+                            quadruples.append(quad)
+                            our_else = ('ELSE', parent[1])
+                            if len(tree.data[our_else]) > 0:
+                                
+                                
+                            index = parent[1] + 1
+                            
+                            
+                    
+                            
                         
                         
+                    
+                                        
         
         print("index going to {}".format(index))
     print("Quadruples: ", quadruples)
