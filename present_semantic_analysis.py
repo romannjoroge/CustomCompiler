492eb6f9401b2b9b8112d677be0b6fdb402ba7d9
mbeka02 <antonymbeka@gmail.com>
2024-05-31 09:51:06 +0300

    feat: added store

--- present_semantic_analysis.py
+++ present_semantic_analysis.py
@@ -1,9 +1,13 @@
 from allIdentifiersMustBeDefined import allIdentifiersMustBeDefined
 from func_types import typesinCallmatchDef
 from scanner import Scanner
+from store import storeTypesData
 from parser_1 import parserv2
 
 tokens = Scanner()
+identifier_metadata,function_metadata=storeTypesData(tokens)
+print(" collected identifier information=>",identifier_metadata)
+print("Collected function information=>",function_metadata)
 tree, labels = parserv2(tokens, [])
 allIdentifiersMustBeDefined(tree=tree, tokens=tokens)
 typesinCallmatchDef()
