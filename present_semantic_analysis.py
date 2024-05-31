a023669c6014ea6a60f4fa1e63ce359163239794
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-05-31 09:38:34 +0300

    feat: types in call should match def

--- present_semantic_analysis.py
+++ present_semantic_analysis.py
@@ -1,7 +1,9 @@
 from allIdentifiersMustBeDefined import allIdentifiersMustBeDefined
+from func_types import typesinCallmatchDef
 from scanner import Scanner
 from parser_1 import parserv2
 
 tokens = Scanner()
 tree, labels = parserv2(tokens, [])
 allIdentifiersMustBeDefined(tree=tree, tokens=tokens)
+typesinCallmatchDef()
